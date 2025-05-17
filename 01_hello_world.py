67204fecb5579ae65a963f0b8bb69d798ed06e42
ajitendra363 <ajitendra363@gmail.com>
2025-05-17 14:58:06 +0530

    hello world program

---
+++ 01_hello_world.py
@@ -0,0 +1,5 @@
+# This is a comment
+# The following line will print "Hello World!" to the console
+print("Hello World!")  
+# This will also print "Hello World!" to the console
+# The following line will print "Hello World!" to the consolegt
